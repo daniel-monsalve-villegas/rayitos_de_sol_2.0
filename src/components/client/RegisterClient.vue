296136486d2ada4eefb6f04f6698aabdf4fd392e
Jill Angel <tpennywise3@hotmail.com>
2025-03-02 02:47:53 -0500

    refactor

--- src/components/client/RegisterClient.vue
+++ src/components/client/RegisterClient.vue
@@ -93,7 +93,7 @@ onMounted(async () => {
 })
 
 const submitForm = async () => {
-  isSubmitting.value = true // Cambia el estado a "Enviando..." y deshabilita el botón
+  isSubmitting.value = true 
 
   const payload = {
     nameClient: formData.value.firstName,
